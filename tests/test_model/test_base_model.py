008e3c2cf2eb909d6d688320fb18b52cc2417a70
truth <truth@gmail.com>
2024-01-27 14:11:37 -0800

    base model

--- tests/test_model/test_base_model.py
+++ tests/test_model/test_base_model.py
@@ -12,18 +12,17 @@ class testBaseModel(unittest.TestCase):
         my_model = BaseModel()
         initial_update = my_model.updated_at
         current_update = my_model.save
-        self.assertIsNotEqual(initial_update,current_update)
+        self.assertNotEqual(initial_update,current_update)
     def test_to_dict(self):
         my_model = BaseModel()
-        my_model_dict = my_model.to_dict
-        self.assertIsInstance(my_model_dict,dict)
-        self.assertIsEqual(my_model_dict['__class__'],'BaseModel')
-        self.assertIsEqual(my_model_dict['id'],my_model.id)
-        self.assertIsEqual(my_model_dict['created_at'],my_model.created_at.isoformat())
-        self.assertIsEqual(my_model_dict['updated-at'],my_model.updated_at.isoformat())
+        my_model_dict = my_model.to_dict()
+        self.assertEqual(my_model_dict['class'],'BaseModel')
+        self.assertEqual(my_model_dict['id'],my_model.id)
+        self.assertEqual(my_model_dict['created_at'],my_model.created_at.isoformat())
+        self.assertEqual(my_model_dict['updated_at'],my_model.updated_at.isoformat())
     def test_str(self):
         my_model = BaseModel()
-        self.assertTrue(str(my_model).startswith(['BaseModel']))
+        self.assertTrue(str(my_model).startswith("[BaseModel]"))
         self.assertIn(my_model.id,str(my_model))
         self.assertIn(str(my_model.__dict__),str(my_model))
 if __name__ =="__main__":
