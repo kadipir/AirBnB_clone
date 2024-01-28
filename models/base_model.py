c5e5ab3cd717cfbc2495ec5df418c4bd1ac0ecbe
truth <truth@gmail.com>
2024-01-28 13:52:26 -0800

    updated base model

--- models/base_model.py
+++ models/base_model.py
@@ -5,11 +5,21 @@ from datetime import datetime
 import uuid
 class BaseModel:
     """this is a class that defines all common attributes of other classes"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """method used to assign uuid when an instance is created"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
+        time_format = "%y-%m-%dT%H:%M%s.%f"
+        if kwargs:
+            for key,value in kwargs.item:
+                if key == '__class__':
+                    continue
+                elif key == 'created_at' or key == 'updated_at':
+                    setattr(self, key ,datetime.strptime(value,time_format))
+                else:
+                    setattr(self, key.value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
     def save(self):
         """updates the current time an instance was saved"""
         self.updated_at = datetime.utcnow()
